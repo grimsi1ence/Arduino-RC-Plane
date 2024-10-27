4a307c1e66cd525fd517bfb88427da14696d8699
grimsi1ence <144237328+grimsi1ence@users.noreply.github.com>
2024-10-27 12:16:53 +0200

    Add files via upload

---
+++ motor_2.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+int joy = A3;
+Servo motor;
+int mot_pin = 9;//Подключаем левый мотор к пину 9
+int nach = 1500;//Начальная позиция, всегда 1.5 мс для регуляторов бесколлекторных двигателей
+void setup() {
+  pinMode(joy,INPUT);
+  motor.attach(mot_pin, nach, 2300);//Максимальное значение ШИМ 2.3 мс
+  Serial.begin(9600);}
+
+void loop() {
+  nach = analogRead(joy);
+  nach = map(nach,0,1023,10,130);
+  Serial.println(nach);
+  motor.write(nach);
+  delay(20);
+}
